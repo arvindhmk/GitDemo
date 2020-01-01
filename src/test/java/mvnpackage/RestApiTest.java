4bf5d11a03b00185345a06138aa1b0f41ec271f1
arvindh <mkarvindh@gmail.com>
2020-01-02 04:46:29 +0530

    commited modification

--- src/test/java/mvnpackage/RestApiTest.java
+++ src/test/java/mvnpackage/RestApiTest.java
@@ -7,7 +7,10 @@ public class RestApiTest
 	@Test
 	public void postjira()
 	{
-	System.out.println("Rest - JIRA");	
+	System.out.println("Rest - JIRA");
+	System.out.println("Post - JIRA");
+	System.out.println("Post - JIRA");
+	System.out.println("Post - JIRA");	
 	}
 	@Test
 	public void deleteTwitter()
